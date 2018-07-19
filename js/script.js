e00e2e50d67a97963294c1cb9484ed0d29ab8bb3
Ewa Mikuła <ewamariamikula@gmail.com>
2018-07-19 18:45:21 +0200

    Init project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function randomString() { // losowanie randomowego stringa do id
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+});
